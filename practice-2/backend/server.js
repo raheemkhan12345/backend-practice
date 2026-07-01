7a8ccb5b1a873ba655d4430663346f6da65f058e
Abdul Rahim <raheemkhan.rk12345@gmail.com>
2026-07-01 23:55:39 +0500

    update

--- practice-2/backend/server.js
+++ practice-2/backend/server.js
@@ -3,10 +3,42 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Server is ready')
-})
+    res.send('Server is ready');
+});
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'joke 1',
+            content: 'this is a joke'
+        },
+        {
+            id: 2,
+            title: 'joke 2',
+            content: 'this is another joke'
+        },
+        {
+            id: 3,
+            title: 'joke 3',
+            content: 'this is a third joke'
+        },
+        {
+            id: 4,
+            title: 'joke 4',
+            content: 'this is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'joke 5',
+            content: 'this is a fifth joke'
+        }
+    ];
+
+    res.send(jokes);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`server is running on localhost:${PORT}`)
-})
+    console.log(`server is running on localhost:${PORT}`);
+});
