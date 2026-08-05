2355a52368f79b7a7032c5c5e5fa28063409c33b
Abdul Rahim <raheemkhan.rk12345@gmail.com>
2026-08-05 10:18:21 +0500

    Create ApiError.js

---
+++ practice-3/src/utils/ApiError.js
@@ -0,0 +1,14 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message =  "something went wrong",
+        
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        errors = [];
+    }
+}
