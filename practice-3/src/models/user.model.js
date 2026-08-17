6c01e2bba9d9d79396a49c947d9ed1b45d86af79
Abdul Rahim <raheemkhan.rk12345@gmail.com>
2026-08-17 17:01:40 +0500

    update

--- practice-3/src/models/user.model.js
+++ practice-3/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
     {
@@ -50,5 +52,17 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 )
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password'))
+        return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isModifiedpasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model('User', userSchema);
--- practice-3/src/models/video.model.js
+++ practice-3/src/models/video.model.js
@@ -1,16 +1,50 @@
-import mongoose, {Schema} from "mongoose";
- 
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
 const videoSchema = new mongoose.Schema(
     {
         videoFile: {
             type: String, // use cloudinary url.
             required: true,
+        },
+        thumbnail: {
+            type: String, // use cloudinary url.
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+
+        },
+        duration: {
+            type: String,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            boolean: true,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
         }
     },
     {
         timestamps: true,
     }
 )
+
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 
 export const Video = mongoose.model('Video', videoSchema);
